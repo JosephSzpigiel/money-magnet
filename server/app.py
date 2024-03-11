3e78ee929e5f73277e90b6d91a0fc6ecb1c04691
Joseph Szpigiel <josephszpigiel@gmail.com>
2024-03-11 16:27:48 -0400

    Set up get and post for users

--- server/app.py
+++ server/app.py
@@ -3,19 +3,32 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, session
 from flask_restful import Resource
+import datetime
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import User, Item
+from models import User, Item, Account, Transaction
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class Users(Resource):
+    def get(self):
+        return make_response([user.to_dict() for user in User.query.all()], 200)
+    def post(self):
+        params = request.json
+        new_user = User(
+            email = params['email'],
+            password = params['password']
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_email'] = new_user.email
+        return make_response({'user': new_user.to_dict()}, 201)
+
+api.add_resource(Users, '/users')
 
 
 if __name__ == '__main__':
